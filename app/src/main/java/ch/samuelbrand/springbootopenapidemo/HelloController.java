287f80658b4dd569310efc9a6fd8f19a286d3d00
Samuel Brand <saemu@users.noreply.github.com>
2021-02-05 14:59:46 +0100

    add new endpoint with POST request

--- app/src/main/java/ch/samuelbrand/springbootopenapidemo/HelloController.java
+++ app/src/main/java/ch/samuelbrand/springbootopenapidemo/HelloController.java
@@ -1,6 +1,8 @@
 package ch.samuelbrand.springbootopenapidemo;
 
 import ch.samuelbrand.api.HelloApi;
+import ch.samuelbrand.model.Greeting;
+import ch.samuelbrand.model.Person;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
@@ -15,5 +17,12 @@ public class HelloController implements HelloApi {
     @Override
     public ResponseEntity<String> helloName(String name) {
         return ResponseEntity.ok("Hello " + name);
+    }
+
+    @Override
+    public ResponseEntity<Greeting> helloPost(Person person) {
+        final Greeting greeting = new Greeting();
+        greeting.setText(String.format("Hello %s %s %s", person.getTitle(), person.getFirstname(), person.getLastname()));
+        return ResponseEntity.ok(greeting);
     }
 }
