4425e022fb60861bf60732a408c3b3bd0d5f8892
Samuel Brand <saemu@users.noreply.github.com>
2021-02-08 16:46:30 +0100

    add some unittesta using spock framework

---
+++ app/src/test/groovy/ch/samuelbrand/springbootopenapidemo/HelloControllerTest.groovy
@@ -0,0 +1,56 @@
+package ch.samuelbrand.springbootopenapidemo
+
+import ch.samuelbrand.model.Person
+import org.springframework.http.HttpStatus
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class HelloControllerTest extends Specification {
+
+    def "hello"() {
+        when:
+        final def controller = new HelloController()
+        final def request = controller.hello()
+
+        then:
+        HttpStatus.OK == request.statusCode
+        "Hello World" == request.body
+    }
+
+    def "helloName"() {
+        given:
+        final def controller = new HelloController()
+        final def name = "Samuel"
+
+        when:
+        final def request = controller.helloName(name)
+
+        then:
+        HttpStatus.OK == request.statusCode
+        "Hello Samuel" == request.body
+    }
+
+    def "helloPost"() {
+        given:
+        final def controller = new HelloController()
+        final def person = new Person()
+        person.setFirstname(firstname)
+        person.setLastname(lastname)
+        person.setTitle(title)
+
+        when:
+        final def request = controller.helloPost(person)
+
+        then:
+        HttpStatus.OK == request.statusCode
+        final def expected = String.format("Hello %s %s %s", title, firstname, lastname)
+        expected == request.body.getText()
+
+        where:
+        firstname | lastname | title
+        "James"   | "Bond"   | "Mr."
+        "Daisy"   | "Duck"   | "Mrs."
+
+    }
+}
